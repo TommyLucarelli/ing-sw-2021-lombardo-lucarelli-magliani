d9318fee275d33f1efd5df6db60e64becb9708bb
giacomolmb <giacomo.lombardo@mail.polimi.it>
2021-03-22 15:22:07 +0100

    Packages setup.

---
+++ java/src/main/java/it/polimi/ingsw/core/Game.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class Game
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
