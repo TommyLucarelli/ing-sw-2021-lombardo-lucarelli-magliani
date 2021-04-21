38ca3c4faab2d5dffac2abecbc2ab9586accc5be
TommyLucarelli <TommyLucarelli@users.noreply.github.com>
2021-04-21 16:23:18 +0200

    First implementation of LeaderCardHandler

--- src/main/java/it/polimi/ingsw/core/controller/LeaderCardHandler.java
+++ src/main/java/it/polimi/ingsw/core/controller/LeaderCardHandler.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.core.controller;
 
+import it.polimi.ingsw.core.model.Flag;
 import it.polimi.ingsw.core.model.LeaderCard;
 import it.polimi.ingsw.core.model.Resource;
 import it.polimi.ingsw.core.model.ResourceQty;
@@ -28,26 +29,47 @@ public class LeaderCardHandler implements PhaseHandler{
             if (action) {
                 lc = controller.getCurrentPlayer().getBoard().getLeader(lcID);
                 check = checkRequirements(lc);
+                if(check){
+                    lc.setAbilityActivation();
+                    controller.getCurrentPlayer().getBoard().setAbilityActivationFlag(lc.getSpecialAbility().getAbilityType(), lcID);
+                }
             } else {
                 controller.getCurrentPlayer().getBoard().removeLeaderCard(controller.getCurrentPlayer().getBoard().getLeader(lcID));
                 controller.getCurrentGame().FaithTrackUpdate(controller.getCurrentPlayer(), 1, 0);
             }
-
         }while(!check);
 
         return true;
     }
 
     public boolean checkRequirements(LeaderCard lc){
+        int check1, check2;
         Resource r;
+        ArrayList<Flag> flags = lc.getRequiredFlags();
+
         switch (lc.getSpecialAbility().getAbilityType()){
             case 0: //controllo nel warehouse e nello strongbox se ho 5 risorse del tipo r
 
             case 1: //2 carte di una bandiera e 1 di un altra
-
+                check1 = controller.getCurrentPlayer().getBoard().countFlags(flags.get(0), false);
+                check2 = controller.getCurrentPlayer().getBoard().countFlags(flags.get(1), false);
+                if(check1 > 1 && check2 > 0)
+                    return true;
+                else
+                    return false;
             case 2: //1 carta di una bandiera e 1 di un altra
-
+                check1 = controller.getCurrentPlayer().getBoard().countFlags(flags.get(0), false);
+                check2 = controller.getCurrentPlayer().getBoard().countFlags(flags.get(1), false);
+                if(check1 > 0 && check2 > 0)
+                    return true;
+                else
+                    return false;
             case 3: //1 carta di una bandiera, ma di livello 2
+                check1 = controller.getCurrentPlayer().getBoard().countFlags(flags.get(0), true);
+                if(check1 > 0)
+                    return true;
+                else
+                    return false;
         }
         return true;
     }
