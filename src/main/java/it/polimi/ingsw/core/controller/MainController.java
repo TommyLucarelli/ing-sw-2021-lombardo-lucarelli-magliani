71a097cfa671861ae50bde54dd65920d0625107f
TommyLucarelli <TommyLucarelli@users.noreply.github.com>
2021-04-22 16:30:31 +0200

    Just some thoughts about the flow of the game

--- src/main/java/it/polimi/ingsw/core/controller/MainController.java
+++ src/main/java/it/polimi/ingsw/core/controller/MainController.java
@@ -8,7 +8,7 @@ public class MainController{
 
     private Game currentGame;
     private Player currentPlayer;
-    private ArrayList<Player> players;
+    private ArrayList<String> playerNames; //shuffle this and create the turn order
 
 
     public MainController()
@@ -17,12 +17,27 @@ public class MainController{
     }
 
     public void run(){
-        //inizio gioco + evoluzione turni
+        /**lancia beginHandler e lo itera per gli x player
+        manda il messaggio UPDATE  a tutti i client con lo stato iniziale del gioco
+        Iniziano i turni (verranno eseguiti in un ciclo che termina solo con l'ultimo giocatore e se è finito il gioco)
+         DENTRO IL CICLO:
+         1 - scelta gioca carta leader (MESSAGGIO)
+         2 - crea classe LeaderCardHandler (opzionale)
+         3 - scelta azione principale (MESSAGGIO)
+         4 - crea classe di una delle tre azioni scelte (ci deve essere la possibilità di tornare indietro alla scelta)
+         5 - scelta gioca carta leader (MESSAGGIO)
+         6 - crea classe LeaderCardHandler (opzionale)
+         7 - calcola il prossimo giocatore
+         8 - manda a tutti l'UPDATE del gioco + prepara il prossimo giocatore che deve fare il turno
+         FUORI DAL CICLO:
+         Gestione fine partita
+        **/
     }
 
     /*
     public Player computeNextPlayer(Player p){
         //passa il giocatore successivo
+        //se è single mode restituisce Lorenzo che fa il suo turno speciale tipo
     }*/
 
     public Game getCurrentGame() {
