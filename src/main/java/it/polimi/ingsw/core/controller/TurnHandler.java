ed65baaf5212f0fe7430a6782ecfbb34cd938eb4
martina <martina.magliani@gmail.com>
2021-04-24 15:48:34 +0200

    Modified TurnHandler

--- src/main/java/it/polimi/ingsw/core/controller/TurnHandler.java
+++ src/main/java/it/polimi/ingsw/core/controller/TurnHandler.java
@@ -1,5 +1,18 @@
 package it.polimi.ingsw.core.controller;
 
+import it.polimi.ingsw.net.msg.RequestMsg;
+import it.polimi.ingsw.net.msg.ResponseMsg;
+
 public class TurnHandler implements PhaseHandler{
 
+    public RequestMsg leaderCard(ResponseMsg ms){
+        if(ms){
+            //costruzione e invio messaggio leader_Activation
+            return null;
+        }
+        else{
+            //costruzione e invio messaggio main_choice
+            return null;
+        }
+    }
 }
