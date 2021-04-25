84fc6bc361f56f02b5d18696c2b673c9c828e36f
martina <martina.magliani@gmail.com>
2021-04-25 16:33:35 +0200

    Modified TurnHandler Created StartHandler

---
+++ src/main/java/it/polimi/ingsw/core/controller/StartHandler.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.core.controller;
+
+import it.polimi.ingsw.net.msg.RequestMsg;
+import it.polimi.ingsw.net.msg.ResponseMsg;
+
+public class StartHandler {
+
+    public RequestMsg startGame(ResponseMsg ms) {
+        //costruzione e invio messaggio Short_update
+        return null;
+    }
+
+    public RequestMsg join(ResponseMsg ms) {
+        //costruzione e invio messaggio Short_Update
+        return null;
+    }
+
+    public RequestMsg startMatch(ResponseMsg ms) {
+        //costruzione e invio messaggio Short_Update
+        return null;
+    }
+
+    public RequestMsg startResAndLeader(ResponseMsg ms) {
+        //costruzione e invio messaggio Choose_start_resources
+        //costruzione e invio messaggio Choose_start_leaders
+        return null;
+    }
+}
--- src/main/java/it/polimi/ingsw/core/controller/TurnHandler.java
+++ src/main/java/it/polimi/ingsw/core/controller/TurnHandler.java
@@ -3,7 +3,20 @@ package it.polimi.ingsw.core.controller;
 import it.polimi.ingsw.net.msg.RequestMsg;
 import it.polimi.ingsw.net.msg.ResponseMsg;
 
-public class TurnHandler implements PhaseHandler{
+public class TurnHandler {
+
+    public RequestMsg mainChoice(ResponseMsg ms) {
+        if (ms.equals(0)) {
+            //costruzione e invio messaggio Pick
+            return null;
+        } else if (ms.equals(1)) {
+            //costruzione e invio messaggio Choose_Production
+            return null;
+        } else{
+            //costruzione e invio messaggio Choose_DevCard
+            return null;
+        }
+    }
 
     public RequestMsg leaderCard(ResponseMsg ms){
         if(ms.equals(1)){
