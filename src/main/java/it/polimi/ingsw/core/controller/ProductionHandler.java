cc361f828b8c4e5be5140855f017c7fc5d5446b2
martina <martina.magliani@gmail.com>
2021-04-28 12:59:05 +0200

    Update Messages

--- src/main/java/it/polimi/ingsw/core/controller/ProductionHandler.java
+++ src/main/java/it/polimi/ingsw/core/controller/ProductionHandler.java
@@ -1,11 +1,15 @@
 package it.polimi.ingsw.core.controller;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import it.polimi.ingsw.core.model.Board;
 import it.polimi.ingsw.core.model.Resource;
 import it.polimi.ingsw.core.model.ResourceQty;
 import it.polimi.ingsw.net.msg.RequestMsg;
 import it.polimi.ingsw.net.msg.ResponseMsg;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class ProductionHandler {
@@ -23,7 +27,7 @@ public class ProductionHandler {
         //più un recipe per la prod base e due ResourceQty per le special ability
         boolean check = true;
 
-        ArrayList<ResourceQty> inputResources;
+        ArrayList<ResourceQty> inputResources = new ArrayList<>();
         ArrayList<ResourceQty> specialResources = new ArrayList<>();
         ArrayList<ResourceQty> outputResources= new ArrayList<>();
 
@@ -31,36 +35,43 @@ public class ProductionHandler {
         int[] personalResources = board.personalResQtyToArray();
         int[] copyPersonalResources = board.personalResQtyToArray();
 
+        //Resources Array as Response
         ArrayList<Integer> response = new ArrayList<>();
+        Gson gson = new Gson();
+        String json = ms.getPayload().get("response").getAsString();
+        Type collectionType = new TypeToken<ArrayList<Integer>>(){}.getType();
+        response = gson.fromJson(json, collectionType);
 
         for(int i=0; i<response.size(); i++){
             if(response.get(i)==1){
                 //recipe mex
                 //inputResource = recipeBasicProduction.getInputResources();
-                //personalResources = reduceResource(inResources, personalResources);
+                personalResources = reduceResource(inputResources, personalResources);
                 //outputResources.add(recipeBasicProduction.getOutputResources());
             }else if(response.get(i)>=2 && response.get(i)<5){
                 //recipe devcard
                 inputResources = board.getDevCardSlot(response.get(i)-2).getTopCard().getRecipe().getInputResources();
                 personalResources = reduceResource(inputResources, personalResources);
                 outputResources.addAll(board.getDevCardSlot(response.get(i)-2).getTopCard().getRecipe().getOutputResources());
-            } else{
+            }else{
                 //se sono attivate vedi input res
                 if(board.isActivated(i+1) != 0){
                     specialResources.add(new ResourceQty(board.getLeader(board.isActivated(i+1)).getSpecialAbility().getAbilityResource(),1));
                     personalResources = reduceResource(specialResources, personalResources);
                     outputResources.addAll(specialResources);
-                }else {
+                }else{
                     check = false;
                     break;
                 }
             }
         }
+
         for(int i=0; i < personalResources.length; i++){
             copyPersonalResources[i]-=personalResources[i];
             if(personalResources[i] < 0)
                 check = false;
         }
+
         if(check){
             //rimozione risorse
             board.getWarehouse().decResWarehouse(copyPersonalResources, false);
