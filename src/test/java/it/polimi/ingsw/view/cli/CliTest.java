88ce1adaa45b0f268deead9f9ea23ef02d7c7924
martina <martina.magliani@gmail.com>
2021-05-05 19:41:34 +0200

    CliUpdate

--- src/test/java/it/polimi/ingsw/view/cli/CliTest.java
+++ src/test/java/it/polimi/ingsw/view/cli/CliTest.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.view.cli;
 
+import it.polimi.ingsw.core.model.*;
+import it.polimi.ingsw.view.compact.CompactBoard;
 import it.polimi.ingsw.view.compact.CompactMarket;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -10,11 +14,52 @@ public class CliTest {
     @Test
     public void printMarket() {
         Cli cli = new Cli();
-        cli.printMarket();
+        CompactMarket market = new CompactMarket();
+        int[] newMarket = {1, 3, 5, 2, 6, 5, 1, 2, 3, 4, 1, 1, 4};
+        market.setMarket(newMarket);
+        cli.printMarket(market);
     }
 
     @Test
     public void printDevCardSlot() {
+        ArrayList<ResourceQty> cost = new ArrayList<ResourceQty>();
+        cost.add(new ResourceQty(Resource.COIN, 5));
+        cost.add(new ResourceQty(Resource.STONE, 2));
+        ArrayList<ResourceQty> endRes = new ArrayList<ResourceQty>();
+        ArrayList<ResourceQty> startRes = new ArrayList<ResourceQty>();
+        startRes.add(new ResourceQty(Resource.COIN, 1));
+        startRes.add(new ResourceQty(Resource.SHIELD, 1));
+        endRes.add(new ResourceQty(Resource.SERVANT, 2));
+        endRes.add(new ResourceQty(Resource.STONE, 2));
+        endRes.add(new ResourceQty(Resource.FAITH, 1));
+        Recipe recipe = new Recipe(startRes, endRes);
+        DevCard devCard = new DevCard(39, new Flag(1, Colour.GREEN), recipe, cost, 10);
+
+        ArrayList<ResourceQty> cost1 = new ArrayList<ResourceQty>();
+        cost.add(new ResourceQty(Resource.COIN, 5));
+        cost.add(new ResourceQty(Resource.STONE, 2));
+        ArrayList<ResourceQty> endRes1 = new ArrayList<ResourceQty>();
+        ArrayList<ResourceQty> startRes1 = new ArrayList<ResourceQty>();
+        startRes.add(new ResourceQty(Resource.COIN, 1));
+        startRes.add(new ResourceQty(Resource.SHIELD, 1));
+        endRes.add(new ResourceQty(Resource.SERVANT, 2));
+        endRes.add(new ResourceQty(Resource.STONE, 2));
+        endRes.add(new ResourceQty(Resource.FAITH, 1));
+        Recipe recipe1 = new Recipe(startRes1, endRes1);
+        DevCard devCard1 = new DevCard(39, new Flag(2, Colour.YELLOW), recipe1, cost1, 8);
+
+        ArrayList<ResourceQty> cost2 = new ArrayList<ResourceQty>();
+        cost.add(new ResourceQty(Resource.SHIELD, 5));
+        cost.add(new ResourceQty(Resource.STONE, 2));
+        ArrayList<ResourceQty> endRes2 = new ArrayList<ResourceQty>();
+        ArrayList<ResourceQty> startRes2 = new ArrayList<ResourceQty>();
+        startRes.add(new ResourceQty(Resource.COIN, 1));
+        startRes.add(new ResourceQty(Resource.STONE, 1));
+        endRes.add(new ResourceQty(Resource.SERVANT, 2));
+        endRes.add(new ResourceQty(Resource.STONE, 2));
+        endRes.add(new ResourceQty(Resource.FAITH, 1));
+        Recipe recipe2 = new Recipe(startRes2, endRes2);
+        DevCard devCard2 = new DevCard(11, new Flag(3, Colour.BLUE), recipe2, cost2, 5);
     }
 
     @Test
@@ -24,24 +69,57 @@ public class CliTest {
     @Test
     public void printWarehouse() {
         Cli cli = new Cli();
-        cli.printWarehouse();
+        CompactBoard board = new CompactBoard();
+        Resource[] resources = {Resource.COIN,Resource.SHIELD, Resource.SHIELD, Resource.STONE, Resource.STONE, Resource.STONE, Resource.SERVANT, Resource.SERVANT, Resource.ANY, Resource.ANY};
+        board.setWarehouse(resources);
+        cli.printWarehouse(board);
     }
 
     @Test
     public void printStrongbox() {
+        CompactBoard board = new CompactBoard();
+        int[] strongbox = {0,1,3,1};
+        board.setStrongbox(strongbox);
         Cli cli = new Cli();
-        cli.printStrongbox();
+        cli.printStrongbox(board);
     }
 
     @Test
     public void printFaithTrack() {
+        CompactBoard board = new CompactBoard();
+        board.setFaithTrackIndex(10);
         Cli cli = new Cli();
-        cli.printFaithTrack();
+        cli.printFaithTrack(board);
     }
 
     @Test
     public void printPersonalBoard() {
+        CompactBoard board = new CompactBoard();
         Cli cli = new Cli();
-        cli.printPersonalBoard();
+        Resource[] resources = {Resource.COIN,Resource.SHIELD, Resource.SHIELD, Resource.STONE, Resource.STONE, Resource.STONE, Resource.SERVANT, Resource.SERVANT, Resource.ANY, Resource.ANY};
+        board.setWarehouse(resources);
+        int[] strongbox = {0,1,3,1};
+        board.setStrongbox(strongbox);
+        cli.printPersonalBoard(board);
+    }
+
+    @Test
+    public void printDevCard() {
+        ArrayList<ResourceQty> cost = new ArrayList<ResourceQty>();
+        cost.add(new ResourceQty(Resource.COIN, 5));
+        cost.add(new ResourceQty(Resource.STONE, 2));
+        ArrayList<ResourceQty> endRes = new ArrayList<ResourceQty>();
+        ArrayList<ResourceQty> startRes = new ArrayList<ResourceQty>();
+        startRes.add(new ResourceQty(Resource.COIN, 1));
+        startRes.add(new ResourceQty(Resource.SHIELD, 1));
+        endRes.add(new ResourceQty(Resource.SERVANT, 2));
+        endRes.add(new ResourceQty(Resource.STONE, 2));
+        endRes.add(new ResourceQty(Resource.FAITH, 1));
+        Recipe recipe = new Recipe(startRes, endRes);
+        DevCard devCard = new DevCard(39, new Flag(1, Colour.GREEN), recipe, cost, 10);
+
+        Cli cli =new Cli();
+        cli.printDevCard(devCard);
+
     }
 }
