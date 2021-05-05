04676c63a4f9885e87cd59dd874a251a53ff7043
TommyLucarelli <TommyLucarelli@users.noreply.github.com>
2021-05-05 14:55:30 +0200

    little update

--- src/main/java/it/polimi/ingsw/view/compact/CompactPlayer.java
+++ src/main/java/it/polimi/ingsw/view/compact/CompactPlayer.java
@@ -1,4 +1,13 @@
 package it.polimi.ingsw.view.compact;
 
 public class CompactPlayer {
+    private int playerID;
+    private String playerName;
+    private CompactBoard compactBoard;
+
+    public CompactPlayer(int playerID, String playerName){
+        compactBoard = new CompactBoard();
+        this.playerID = playerID;
+        this.playerName = playerName;
+    }
 }
