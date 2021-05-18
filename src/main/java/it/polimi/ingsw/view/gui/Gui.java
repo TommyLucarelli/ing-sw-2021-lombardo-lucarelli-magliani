52ccb6979da58d6eadd0254ba461247b19e0b367
giacomolmb <giacomo.lombardo@mail.polimi.it>
2021-05-18 16:27:30 +0200

    Creation of first scene - let's go!

--- src/main/java/it/polimi/ingsw/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Gui.java
@@ -1,33 +1,20 @@
 package it.polimi.ingsw.view.gui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Gui extends Application {
 
-    private Parent createContent() {
-        Rectangle box = new Rectangle(100, 50, Color.BLUE);
-
-        transform(box);
-
-        return new Pane(box);
-    }
-
-    private void transform(Rectangle box) {
-        box.setTranslateX(100);
-        box.setTranslateY(200);
-    }
-
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent(), 300, 300, Color.GRAY));
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/fxml/sample.fxml"));
+        Parent root = loader.load();
+        stage.setTitle("Masters of Renaissance");
+        stage.setScene(new Scene(root, 400, 300));
         stage.show();
     }
 
