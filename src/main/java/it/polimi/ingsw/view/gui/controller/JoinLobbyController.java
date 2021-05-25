50a78062300f04d59a6580dc3ed6df237ee3983e
giacomolmb <giacomo.lombardo@mail.polimi.it>
2021-05-25 18:28:57 +0200

    Started implementing menu funtionalities.

--- src/main/java/it/polimi/ingsw/view/gui/controller/JoinLobbyController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controller/JoinLobbyController.java
@@ -5,6 +5,7 @@ import it.polimi.ingsw.net.msg.MessageType;
 import it.polimi.ingsw.net.msg.ResponseMsg;
 import it.polimi.ingsw.view.gui.JavaFxApp;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 public class JoinLobbyController {
@@ -20,5 +21,27 @@ public class JoinLobbyController {
         } else {
             lobbyId.setPromptText("Please enter a valid lobby Id!");
         }
+    }
+
+    public void aboutDialog(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setHeaderText(null);
+        alert.setContentText("Progetto di Ingegneria del Software - A.A. 2020/21\n" +
+                "Autori: Martina Magliani, Giacomo Lombardo, Tommaso Lucarelli");
+        alert.showAndWait();
+    }
+
+    public void helpDialog(){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Instructions");
+        alert.setHeaderText(null);
+        alert.setContentText("Enter the lobby ID of the lobby you want to join.\n" +
+                "A valid lobby ID is a number between 10000 and 99999");
+        alert.showAndWait();
+    }
+
+    public void quit(){
+        JavaFxApp.close();
     }
 }
