1f4c5015b50250801800e6a4d152aed44aaccac2
martina <martina.magliani@gmail.com>
2021-05-28 00:19:57 +0200

    gui update

---
+++ src/main/java/it/polimi/ingsw/view/gui/controller/StrongBoxController.java
@@ -0,0 +1,14 @@
+package it.polimi.ingsw.view.gui.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class StrongBoxController{
+    @FXML
+    Button undo;
+
+    public void setUndo(){
+
+    }
+}
