87c2e0c028c1994ca482374a5a489eff1c8999d0
giacomolmb <giacomo.lombardo@mail.polimi.it>
2021-06-05 15:51:44 +0200

    Fixed devcard placement scene.

--- src/main/java/it/polimi/ingsw/view/gui/controller/PlaceDevCardController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controller/PlaceDevCardController.java
@@ -13,10 +13,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.ResourceBundle;
 
 public class PlaceDevCardController implements DynamicController {
     @FXML
@@ -25,7 +23,31 @@ public class PlaceDevCardController implements DynamicController {
     @FXML
     Rectangle r11, r12, r13, r21, r22, r23, r31, r32, r33;
 
-    ArrayList<Rectangle> rectangles = new ArrayList<>();
+
+    @Override
+    public void setData(JsonObject data) {
+        ArrayList<ImageView> slots = new ArrayList<>(Arrays.asList(slot11, slot12, slot13, slot21, slot22, slot23, slot31, slot32, slot33));
+        ArrayList<Rectangle> rectangles = new ArrayList<>(Arrays.asList(r11, r12, r13, r21, r22, r23, r31, r32, r33));
+
+        int[][] mySlots = JavaFxApp.getManager().getMyself().getCompactBoard().getDevCardSlots();
+
+        String json = data.get("freeSpots").getAsString();
+        ArrayList<Integer> freeSpots = (new Gson()).fromJson(json, new TypeToken<ArrayList<Integer>>(){}.getType());
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if(mySlots[i][j] != 0)
+                    slots.get((i * 3) + j).setImage(new Image(getClass().getResourceAsStream("/images/cards/" + mySlots[i][j] + ".png")));
+                else {
+                    if(freeSpots.contains(i)){
+                        rectangles.get((i * 3) + j).setVisible(true);
+                        rectangles.get((i * 3) + j).setDisable(false);
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
     public void place1(){
         choice(1);
@@ -48,31 +70,5 @@ public class PlaceDevCardController implements DynamicController {
 
         Stage stage = (Stage) slot11.getScene().getWindow();
         stage.close();
-    }
-
-    @Override
-    public void setData(JsonObject data) {
-        ArrayList<ImageView> slots = new ArrayList<>(Arrays.asList(slot11, slot12, slot13, slot21, slot22, slot23, slot31, slot32, slot33));
-        rectangles.addAll(Arrays.asList(r11, r12, r13, r21, r22, r23, r31, r32, r33));
-
-        int[][] mySlots = JavaFxApp.getManager().getMyself().getCompactBoard().getDevCardSlots();
-
-        String json = data.get("freeSpots").getAsString();
-        ArrayList<Integer> freeSpots = (new Gson()).fromJson(json, new TypeToken<ArrayList<Integer>>(){}.getType());
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.println(mySlots[i][j]);
-                if(mySlots[i][j] != 0)
-                    slots.get((i * 3) + j).setImage(new Image(getClass().getResourceAsStream("/images/cards/" + mySlots[i][j] + ".png")));
-                else {
-                    if(freeSpots.contains(i)){
-                        rectangles.get((i * 3) + j).setVisible(true);
-                        rectangles.get((i * 3) + j).setDisable(false);
-                        break;
-                    }
-                }
-            }
-        }
     }
 }
